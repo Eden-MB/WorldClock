8f1ea16573524a1d09253ad8a2c67256d91f4ba7
Eden-MB <154504714+Eden-MB@users.noreply.github.com>
2024-02-15 17:56:58 +0300

    updates on html, js, and css

--- index.js
+++ index.js
@@ -16,8 +16,42 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <a href="/" id="link">All cities</a>
   `;
 }
+function updateTime() {
+  let addisAbabaElement = document.querySelector("#addis");
+  if (addisAbabaElement) {
+    let addisDateElement = document.querySelector(".date");
+    let addisTimeElement = document.querySelector(".time");
+    let addisAbaTime = moment().tz("Africa/Addis_Ababa");
+
+    addisDateElement.innerHTML = addisAbaTime.format("MMMM	Do YYYY");
+    addisTimeElement.innerHTML = addisAbaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  let newYorkElement = document.querySelector("#newYork");
+  if (newYorkElement) {
+    let newYorkDateElement = document.querySelector(".date2");
+    let newYorkTimeElement = document.querySelector(".time2");
+    let newYorkTime = moment().tz("America/New_York");
+  
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[<small>]"
+    );
+  }
+  }
+
+
+function updateTime2() {
 
 let chooseCityElement = document.querySelector("#chooseCity");
 chooseCityElement.addEventListener("change", updateCity);
+
+updateTime();
+setInterval(updateTime, 1000);
+
+updateTime2();
+setInterval(updateTime, 1000);
